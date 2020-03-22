ff6715c2077f6b2d3d1aec2d743b24b82c6b55c7
viistorrr <victormeza41@gmail.com>
2020-03-21 19:19:10 -0500

    add userguest view

--- app/screens/Account/MyAccount.js
+++ app/screens/Account/MyAccount.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import * as firebase from "firebase";
+import Loading from "../../components/Loading";
+import UserGuest from "./UserGuest";
+import UserLogged from "./Userlogged";
 
 export default function MyAccount() {
   const [login, setLogin] = useState(null);
@@ -12,23 +15,8 @@ export default function MyAccount() {
   }, []);
 
   if (login === null) {
-    return (
-      <View>
-        <Text>Cargando...</Text>
-      </View>
-    );
+    return <Loading isVisible={true} text="Cargando..." />;
   }
 
-  if (login) {
-    return (
-      <View>
-        <Text>usuario Logeado...</Text>
-      </View>
-    );
-  }
-  return (
-    <View>
-      <Text>usuario NO Logeado...</Text>
-    </View>
-  );
+  return login ? <UserLogged /> : <UserGuest />;
 }
