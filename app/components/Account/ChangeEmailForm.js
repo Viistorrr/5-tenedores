b7725665ff71ba145b4e10e2b61f0f79b56b20c4
viistorrr <victormeza41@gmail.com>
2020-03-24 20:01:23 -0500

    creando form de change nombre

---
+++ app/components/Account/ChangeEmailForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ChangeEmailForm() {
+  return (
+    <View>
+      <Text>Text from change Email</Text>
+    </View>
+  );
+}
